a4fe32d0abd4c368ccc0ccb03e6cb623b9337253
me6u6 <me6u6y@gmail.com>
2024-07-02 20:32:20 +0900

    []

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use chrono::prelude::*;
+use chrono_tz::{Asia::Tokyo, Tz};
 use cron::Schedule;
 use std::{
     future::Future,
@@ -20,7 +21,8 @@ fn ctrl_c_handler(token: CancellationToken) -> JoinHandle<()> {
 fn make_looper<F1, F2>(
     token: CancellationToken,
     expression: &'static str,
-    f: impl Fn(&DateTime<Utc>) -> F1 + Send + Sync + 'static,
+    // f: impl Fn(&DateTime<Utc>) -> F1 + Send + Sync + 'static,
+    f: impl Fn(&DateTime<Tz>) -> F1 + Send + Sync + 'static,
     g: impl Fn() -> F2 + Send + Sync + 'static,
 ) -> JoinHandle<()>
 where
@@ -29,20 +31,23 @@ where
 {
     spawn(async move {
         let schedule = Schedule::from_str(expression).unwrap();
-        let mut next_tick = schedule.upcoming(Utc).next().unwrap();
+        // let mut next_tick = schedule.upcoming(Utc).next().unwrap();
+        let mut next_tick = schedule.upcoming(Tokyo).next().unwrap();
         loop {
             if token.is_cancelled() {
                 g().await;
                 break;
             }
 
-            let now = Utc::now();
+            // let now = Utc::now();
+            let now = Utc::now().with_timezone(&Tokyo);
             if now >= next_tick {
                 // 指定時刻になったら処理を実行
                 f(&now).await;
 
                 // 次回の実行時刻を計算
-                next_tick = schedule.upcoming(Utc).next().unwrap();
+                // next_tick = schedule.upcoming(Utc).next().unwrap();
+                next_tick = schedule.upcoming(Tokyo).next().unwrap();
             }
 
             // 次回の実行までの待機時間を計算
